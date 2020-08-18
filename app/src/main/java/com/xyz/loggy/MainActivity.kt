d24bc2a5ed19424ba844bcfc60f3f75d85a87a86
imran <imran@able.do>
2020-08-19 02:29:20 +0530

    init

---
+++ app/src/main/java/com/xyz/loggy/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.xyz.loggy
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.xyz.loggy.databinding.ActivityMainBinding
+import com.xyz.simple.SimpleData
+import com.xyz.simple.SimpleServiceGrpcKt
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asExecutor
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
+import timber.log.Timber
+import java.net.URL
+import kotlin.concurrent.fixedRateTimer
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    private val simple by lazy { SimpleServiceGrpcKt.SimpleServiceCoroutineStub(channel()) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        fixedRateTimer("hello", false, 0, 1000) {
+            sendMessage()
+        }
+    }
+
+    private fun sendMessage() {
+        lifecycleScope.launch {
+            val request = flow<SimpleData> {
+                emit(SimpleData.newBuilder().setMsg("Hello Loggy!").build())
+            }
+            simple.simpleRPC(request)
+                .collect {
+                    Timber.d("$it")
+                }
+        }
+    }
+
+    private fun channel(): ManagedChannel {
+        val url = URL("http://192.168.1.7:50111")
+        val port = if (url.port == -1) url.defaultPort else url.port
+
+        Timber.i("Connecting to ${url.host}:$port")
+
+        val builder = ManagedChannelBuilder.forAddress(url.host, port)
+        if (url.protocol == "https") {
+            builder.useTransportSecurity()
+        } else {
+            builder.usePlaintext()
+        }
+
+        return builder.executor(Dispatchers.Default.asExecutor()).build()
+    }
+}
---
+++ app/src/main/java/com/xyz/loggy/MainApplication.kt
@@ -0,0 +1,7 @@
+package com.xyz.loggy
+
+import android.app.Application
+
+class MainApplication : Application() {
+    
+}
