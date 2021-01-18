27b617d93cd7cf14d492de193b8669ce33e67aa5
imran <imohammed23@gmail.com>
2021-01-18 13:57:35 +0530

    seperation of concerns
    - attempt to derive a test driven development

--- sample/src/main/java/loggy/sh/sample/MainApplication.kt
+++ sample/src/main/java/loggy/sh/sample/MainApplication.kt
@@ -14,25 +14,30 @@ import timber.log.Timber
 class MainApplication : Application() {
 
     private val scope = CoroutineScope(Dispatchers.Main)
+    private val loggy = Loggy()
 
     override fun onCreate() {
         super.onCreate()
 
         Timber.plant(Timber.DebugTree())
         scope.launch {
-            Loggy.setup(
+            loggy.setup(
                 this@MainApplication,
                 "bf0b5b86-62f0-4f87-9312-da3eeeceed0f",
                 "dev - "
             )
-            Timber.plant(LoggyTree())
+            Timber.plant(LoggyTree(loggy))
         }
 
-        ProcessLifecycleOwner.get().lifecycle.addObserver(ForegroundBackgroundObserver())
+        ProcessLifecycleOwner.get().lifecycle.addObserver(ForegroundBackgroundObserver(loggy))
+    }
+
+    fun loggyInstance(): Loggy { // TODO: 18/1/21 This needs to go away once we provide a singleton API to access Loggy.
+        return loggy
     }
 }
 
-class ForegroundBackgroundObserver : LifecycleObserver {
+class ForegroundBackgroundObserver(private val loggy: Loggy) : LifecycleObserver {
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun create() {
@@ -41,13 +46,11 @@ class ForegroundBackgroundObserver : LifecycleObserver {
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun start() {
-        Loggy.startSession()
         Timber.d("Application Foreground")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun stop() {
         Timber.d("Application Background")
-        Loggy.endSession()
     }
 }
